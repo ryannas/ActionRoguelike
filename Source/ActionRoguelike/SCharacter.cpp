45f6697e8d7a68d984fb954fa970ffd78a014fb5
ruiyangzhao <ruiyangzhao@tencent.com>
2024-02-07 14:10:52 +0800

    add more movement logic

--- Source/ActionRoguelike/SCharacter.cpp
+++ Source/ActionRoguelike/SCharacter.cpp
@@ -4,6 +4,7 @@
 #include "SCharacter.h"
 
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
@@ -16,9 +17,16 @@ ASCharacter::ASCharacter()
 	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
 	// Attach Component to Root
 	SpringArmComponent->SetupAttachment(RootComponent);
+	// Turn On Using Pawn Control Rotation to Enable Pitching
+	SpringArmComponent->bUsePawnControlRotation = true;
 
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
 	CameraComponent->SetupAttachment(SpringArmComponent);
+
+	// Avoid Unnatural Rotation of Character
+	bUseControllerRotationYaw = false;
+	
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 }
 
 // Called when the game starts or when spawned
@@ -30,7 +38,26 @@ void ASCharacter::BeginPlay()
 
 void ASCharacter::MoveForward(float Value)
 {
-	AddMovementInput(GetActorForwardVector(), Value);
+	FRotator ControlRotator = GetControlRotation();
+	// We Only Need Yaw Rotation
+	ControlRotator.Pitch = 0.0f;
+	ControlRotator.Roll = 0.0f;
+	
+	AddMovementInput(ControlRotator.Vector(), Value);
+}
+
+void ASCharacter::MoveRight(float Value)
+{
+	FRotator ControlRotator = GetControlRotation();
+	ControlRotator.Pitch = 0.0f;
+	ControlRotator.Roll = 0.0f;
+
+	// X -> Forward
+	// Y -> Right
+	// Z -> Up
+	const FVector RightVector = FRotationMatrix(ControlRotator).GetScaledAxis(EAxis::Y);
+	
+	AddMovementInput(RightVector, Value);
 }
 
 // Called every frame
@@ -47,8 +74,10 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 
 	// ASCharacter::MoveForward is Implemented by Ourselves
 	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASCharacter::MoveRight);
 
 	// AddControllerYawInput is Implemented by Pawn
-	PlayerInputComponent->BindAxis("Turn", this, &ASCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
